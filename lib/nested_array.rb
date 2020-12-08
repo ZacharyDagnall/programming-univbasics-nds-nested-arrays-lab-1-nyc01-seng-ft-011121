eb027507579d5448b192cb30222103e53288d873
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-08 19:33:48 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -20,27 +20,11 @@ def assembled_matrix
   storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 end
 
-def sort(arr)
-  arr2 = []
-  
-  for i in 0...(arr.length-1) do
-    arr2[i] = arr[i]
-    
-    for j in 1...arr.length do
-      if(arr[j] < arr2[i]) then
-        arr2[i] = arr[j]
-      end
-    end 
-  end
-  
-  arr=arr2
-  
-end 
 
 def sorted_matrix
   
   sorted_organic = ORGANIC_PRODUCE.sort
-  sorted_conventional = CONVENTIONAL_PRODUCE.sort!
+  sorted_conventional = CONVENTIONAL_PRODUCE.sort
   
   storage_room = [sorted_conventional, sorted_organic]
 end
